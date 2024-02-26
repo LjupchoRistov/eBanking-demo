8c3347fc009595bdacf938b5bf921e264f646f3c
mmate <mmatej.minoski@gmail.com>
2024-02-26 14:24:57 +0100

    Trying to implement Captcha

---
+++ src/main/java/com/ebanking/service/impl/RecaptchaResponseServiceImpl.java
@@ -0,0 +1,27 @@
+package com.ebanking.service.impl;
+
+import com.ebanking.models.RecaptchaResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class RecaptchaResponseServiceImpl {
+    private static final String GOOGLE_RECAPTCHA_ENDPOINT = "https://www.google.com/recaptcha/api/siteverify";
+    private final String RECAPTCHA_SECRET = "6LcvWYApAAAAAN12-yGevOuphwwnIJltxsqeCZXs";
+    public boolean validateCaptcha(String captchaResponse){
+        RestTemplate restTemplate = new RestTemplate();
+
+        MultiValueMap<String, String> requestMap = new LinkedMultiValueMap<>();
+        requestMap.add("secret", RECAPTCHA_SECRET);
+        requestMap.add("response", captchaResponse);
+
+        RecaptchaResponse apiResponse = restTemplate.postForObject(GOOGLE_RECAPTCHA_ENDPOINT, requestMap, RecaptchaResponse.class);
+        if(apiResponse == null){
+            return false;
+        }
+
+        return Boolean.TRUE.equals(apiResponse.isSuccess());
+    }
+}
